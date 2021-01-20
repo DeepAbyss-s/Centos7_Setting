1f1fd75307ab2a68f9d0c219a98579da8af80847
DeepAbyss-s <68440516+DeepAbyss-s@users.noreply.github.com>
2021-01-21 04:53:10 +0900

    Create Python3.7_Cent.sh

---
+++ Python3.7_Cent.sh
@@ -0,0 +1,35 @@
+echo "Python 3.7 version install Start"
+sudo yum install gcc openssl-devel  libffi-devel bzip2-devel
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+tar xzf Python-3.7.3.tgz
+cd Python-3.7.3
+./configure --enable-optimizations
+sudo make altinstall
+sudo ln -s /usr/local/bin/python3.7 /bin/python3
+
+echo "Docker install Start"
+sudo yum remove docker \
+  docker-client \
+  docker-client-latest \
+  docker-common \
+  docker-latest \
+  docker-latest-logrotate \
+  docker-logrotate \
+  docker-engine
+  
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+  
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+sudo systemctl start docker
+
+echo "Nvidia Docker install Start"
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+
+sudo yum install -y nvidia-container-toolkit
+sudo systemctl restart docker
